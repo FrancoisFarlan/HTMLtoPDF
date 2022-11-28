9f77b478655841105280c8e609138baff115cb70
Francois Farlan <francois.farlan@gmail.com>
2022-11-28 14:23:59 +0100

    project working

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,6 +5,9 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Knp\Snappy\Pdf;
+use Knp\Bundle\SnappyBundle\Snappy\Response\PdfResponse;
+
 
 class MainController extends AbstractController
 {
@@ -15,4 +18,29 @@ class MainController extends AbstractController
     {
         return $this->render('main/index.html.twig');
     }
+
+    /**
+     * @Route("/generate", name="generate")
+     */
+    public function generate(Pdf $pdf) :PdfResponse
+    {
+//        Generation d'un PDF à partir d'un template HTML : Le fichier PDF est directement téléchargé sur le PC
+        $nom = "FARLAN";
+        $prenom = "François";
+        $societe = "Google";
+
+        $html = $this->renderView('main/template.html.twig', [
+            'nom'  => $nom,
+            'prenom' => $prenom,
+            'societe' =>$societe,
+        ]);
+
+        return new PdfResponse(
+            $pdf->getOutputFromHtml($html),
+            'file1.pdf'
+        );
+
+
+    }
+
 }
